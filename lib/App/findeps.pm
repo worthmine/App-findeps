334c5151a8bad790d148e95bdaae8cbfac4425c9
worthmine <worthmine@gmail.com>
2020-09-26 08:22:04 +0900

    added if into regex

--- lib/App/findeps.pm
+++ lib/App/findeps.pm
@@ -65,8 +65,8 @@ sub scan_line {
         push @names, split /\s+/, $1;
     } elsif (/use\s+(?:base|parent|autouse)\s+(['"])?($qr4name)\1?/) {
         $names[0] = $2;
-    } elsif (/eval\s*(['"{])\s*(require|use)\s+($qr4name).*(?:\1|})/) {
-        warn "$3 is $2d inside of eval";
+    } elsif (/(eval|if)\s*(['"{])\s*(require|use)\s+($qr4name).*(?:\1|})/) {
+        warn "$4 is $3d inside of $1";
     } elsif (/^\s*(?:require|use)\s+($qr4name)/) {
         $names[0] = $1;
     } elsif (/^\s*require\s+(["'])($qr4name)\.p[lm]\1/) {
