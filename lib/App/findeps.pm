aef56ff10919b07dd4b25f4db3436e0de3a9c6f6
worthmine <worthmine@gmail.com>
2020-10-10 16:42:37 +0900

    became a little faster

--- lib/App/findeps.pm
+++ lib/App/findeps.pm
@@ -119,8 +119,8 @@ sub scan_line {
         next unless length $name;
         next if exists $pairs->{$name};
         next if $name eq 'Plack::Builder';
-        next if $Upgrade and Module::CoreList->is_core($name);
         next if first { $name eq $_ } @pragmas;
+        next if $Upgrade and Module::CoreList->is_core($name);
         $pairs->{$name} = get_version($name);
     }
     return %$pairs;
